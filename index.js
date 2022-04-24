b4430a6c13a384040396f54db9ccfdd40e002f40
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-04-25 00:00:32 +0100

    Add localstorage module

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { DateTime } from "./modules/luxon.js"
 import * as navigation from "./modules/navigation.js";
+import { storage } from "./modules/localstorage.js";
 
 //Date section
     const dateSection = document.getElementById('date');
     dateSection.innerText = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+
+
---
+++ modules/localstorage.js
@@ -0,0 +1,4 @@
+ // Data Storage
+ export let storage=books=>localStorage.setItem('books', JSON.stringify(books));
+  
+
